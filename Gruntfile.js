1bc33eee04952f587852dcec3b3ec8043ec60450
TejaswiR <tejaswi.ramisetty@gmail.com>
2015-08-25 13:22:15 +0530

    final psd

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,9 @@ module.exports = function(grunt) {
           'assets/stylesheets/styles.css': 'assets/stylesheets/src/styles.scss' ,
            'assets/stylesheets/smartphone.css': 'assets/stylesheets/src/smartphone.scss',
            'assets/stylesheets/info.css': 'assets/stylesheets/src/info.scss',
-           'assets/stylesheets/features.css': 'assets/stylesheets/src/features.scss'   
+           'assets/stylesheets/features.css': 'assets/stylesheets/src/features.scss',
+           'assets/stylesheets/ending.css': 'assets/stylesheets/src/ending.scss',
+           'assets/stylesheets/footer.css': 'assets/stylesheets/src/footer.scss'      
         }
       }
     },
@@ -19,11 +21,13 @@ module.exports = function(grunt) {
      cssmin: {
           target: {
           files: {
-            'assets/stylesheets/app.min.css': [
-            'assets/stylesheets/styles.css',
+             'assets/stylesheets/app.min.css': [
+             'assets/stylesheets/styles.css',
              'assets/stylesheets/smartphone.css',
              'assets/stylesheets/info.css',
-             'assets/stylesheets/features.css'
+             'assets/stylesheets/features.css',
+             'assets/stylesheets/ending.css',
+              'assets/stylesheets/footer.css'
              ]
           }
         }
